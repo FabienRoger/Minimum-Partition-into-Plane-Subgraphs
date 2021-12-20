ab989f37f7c54b3e28299dd9cd2b97e3d1bcc3a5
ejlly <ejllythecrafter@gmail.com>
2021-12-20 22:21:27 +0100

    Ajout des classes Point, Graph et d'un makefile pour tout compiler

---
+++ Graph.java
@@ -0,0 +1,11 @@
+class Graph{
+	
+
+	public static void main(String[] args){
+		Point da = new Point(2,3);
+
+
+		System.out.println(da);
+
+	}
+}
---
+++ Point.java
@@ -0,0 +1,18 @@
+class Point{
+	double x,y;
+
+	Point(){
+		x = 0;
+		y = 0;
+	}
+	
+	Point(double px, double py){
+		x = px;
+		y = py;
+	}
+	
+	@Override
+	public String toString(){
+		return "(" + Double.toString(x) + " , " + Double.toString(y) + ")";
+	}
+}
