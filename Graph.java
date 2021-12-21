f28c5a8a9235240fc2e54f83f3fb9321f9b50d88
ejlly <ejllythecrafter@gmail.com>
2021-12-21 12:09:40 +0100

    Classe graphe

--- Graph.java
+++ Graph.java
@@ -5,39 +5,36 @@ import java.util.Map;
 class Graph{
 	
 	int size = 0;
-	HashMap<Point, Integer> vertices; //lookup table pour convertir un point en un entier qui lui est attribué
-	ArrayList<ArrayList<Point>> graph;
+	HashMap<Point, ArrayList<Point>> graph; //lookup table pour convertir un point en un entier qui lui est attribué
 	
+
 	Graph(){
-		vertices = new HashMap<Point, Integer>();
-		graph = new ArrayList<ArrayList<Point>>();
+		graph = new HashMap<Point, ArrayList<Point>>();
 	}
 	
 	void update_vertices(Point a){
 		//ajoute un point au graphe s'il n'existe pas déjà
-		if(!vertices.containsKey(a)){
-			vertices.put(a, size);
-			graph.add(new ArrayList<Point>());
+		if(!graph.containsKey(a)){
+			graph.put(a, new ArrayList<Point>());
 			size++;
 		}
 	}
 
 	void add_arete(Point a, Point b){
-		//ajoute les points s'ils ne sont pas déjà dans le graphe
 		this.update_vertices(a);
 		this.update_vertices(b);
 
 		//relie automatiquement dans les deux sens
-		graph.get(vertices.get(a)).add(b);
-		graph.get(vertices.get(b)).add(a);
+		graph.get(a).add(b);
+		graph.get(b).add(a);
 	}
 
 	@Override
 	public String toString(){
 		String str = "";
-		for(Map.Entry<Point, Integer> depart : vertices.entrySet()){
+		for(Map.Entry<Point, ArrayList<Point>> depart : graph.entrySet()){
 			str += depart.getKey().toString() + " : ";
-			for(Point arrivee : graph.get(depart.getValue())){
+			for(Point arrivee : depart.getValue()){
 				str += arrivee.toString() + " ";
 			}
 			str += "\n";
