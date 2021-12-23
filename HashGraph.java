e3bc429d9c5234134026c1f435293bda7eb8b042
Fabien Roger <efab.roger@gmail.com>
2021-12-23 11:41:57 +0100

    Move main

--- HashGraph.java
+++ HashGraph.java
@@ -46,21 +46,4 @@ class HashGraph extends Graph {
 		// return sol_fct(this);
 		return 0;
 	}
-
-	public static void main(String[] args) {
-		Point p1 = new Point(2, 3);
-		Point p2 = new Point(4, 5);
-		Point p3 = new Point(6, 1);
-
-		Graph g = new HashGraphWithInfo();
-
-		g.add_arete(p1, p2);
-		g.add_arete(p1, p3);
-
-		System.out.println(p1);
-
-		System.out.println("\n");
-		System.out.println(g);
-	}
-
 }
